d678e7f4f0ce4c4ce28e07ac4ae18233fd202c45
Jan Skowronski <crianonim@gmail.com>
2023-08-14 12:12:07 +0100

    supa auth

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+let supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL || "TODO: Your Supabase URL"
+let supabase_key = process.env.NEXT_PUBLIC_SUPABASE_KEY || "TODO: Your Supabase Key"
+
 // Create a single supabase client for interacting with your database
-const supabase = createClient('http://localhost:54321', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0')
+const supabase = createClient(supabase_url,
+    supabase_key)
 
 export default supabase
