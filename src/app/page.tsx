8bbed472c7de480362803559277674a1e61f7117
Jan Skowronski <crianonim@gmail.com>
2023-08-15 14:51:46 +0100

    update state after adding and removing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,17 +39,35 @@ const now: dayjs.Dayjs = dayjs();
 const today: dayjs.Dayjs = dayjs().startOf("day");
 
 export default function Home() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[] | null>(null);
   console.log({ transactions });
   async function insertTransaction() {
     const { data, error } = await supabase
       .from("transactions")
       .insert([{ user_id: "4c647e9a-e2bb-4570-a9f7-0db2e5b4d41f" }])
       .select();
+    console.log("INSERT", { data }, { error });
+
+    if (error || !data?.[0]) {
+      // set error
+      return;
+    } else {
+      const { id, user_id, timestamp } = data[0];
+      setTransactions((tr) =>
+        tr === null ? null : [{ id, user_id, timestamp }, ...tr]
+      );
+    }
   }
 
   async function deleteTransaction(id: number) {
     const { error } = await supabase.from("transactions").delete().eq("id", id);
+    if (!error) {
+      setTransactions((ts) =>
+        ts === null ? null : ts?.filter((t) => t.id != id)
+      );
+    } else {
+      //set error
+    }
   }
 
   const getTransactions = async () => {
@@ -69,43 +87,48 @@ export default function Home() {
   useEffect(() => {
     getTransactions();
   }, []);
-  const canUse = canHaveADrink(transactions);
+  const canUse = transactions !== null && canHaveADrink(transactions);
   return (
     <div className="p-2 flex flex-col gap-2">
       <h2 className="flex justify-center text-lg mb-4">Five-a-Day 😜</h2>
-
-      <p className={`${canUse ? "text-green-500" : "text-red-500"}`}>
-        {canUse
-          ? "We can have a drink"
-          : "We can't have a drink because " +
-            reasonsCantHaveADrink(transactions).join(" and ")}
-      </p>
-      <button
-        onClick={insertTransaction}
-        disabled={!canUse}
-        className={`p-1 rounded border text-lg ${
-          canUse ? "bg-green-500" : "bg-red-500"
-        } text-white`}
-      >
-        {canUse ? "Track use" : "Can't use it now"}
-      </button>
-      <p>Today we had {transactions.length}/5 drinks</p>
-      <ul>
-        {transactions.map((t) => {
-          const tDate = dayjs(t.timestamp);
-          return (
-            <li className="flex gap-1 items-center " key={t.id}>
-              <button
-                className="p-1 border rounded border-red-600 bg-red-600 text-white"
-                onClick={() => deleteTransaction(t.id)}
-              >
-                X
-              </button>
-              {tDate.format("HH:mm")} - {displayDate(tDate)}x{" "}
-            </li>
-          );
-        })}
-      </ul>
+      {transactions === null ? (
+        <p>Loading ...</p>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <p className={`${canUse ? "text-green-500" : "text-red-500"}`}>
+            {canUse
+              ? "We can order a drink! ☕️"
+              : "We can't have a drink 😭, because " +
+                reasonsCantHaveADrink(transactions).join(" and ")}
+          </p>
+          <button
+            onClick={insertTransaction}
+            disabled={!canUse}
+            className={`p-1 rounded border text-lg ${
+              canUse ? "bg-green-500" : "bg-red-500"
+            } text-white`}
+          >
+            {canUse ? "Track use" : "Can't use it now"}
+          </button>
+          <p>Today we had {transactions.length}/5 drinks</p>
+          <ul className="flex flex-col gap-1">
+            {transactions.map((t) => {
+              const tDate = dayjs(t.timestamp);
+              return (
+                <li className="flex gap-1 items-center " key={t.id}>
+                  <button
+                    className="px-1 border rounded border-red-600 bg-red-600 text-white"
+                    onClick={() => deleteTransaction(t.id)}
+                  >
+                    X
+                  </button>
+                  {tDate.format("HH:mm")} - {displayDate(tDate)}x{" "}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
