eed3b0c863e0e8c2ad45a8b40e5fc182676dd62c
Jan Skowronski <crianonim@gmail.com>
2023-08-16 13:37:17 +0100

    add login and last refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect, useState } from "react";
-import dayjs from "dayjs";
+import { useEffect, useState, useTransition } from "react";
+import dayjs, { Dayjs } from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import logo from "../assets/pret-a-manger-logo.png";
+import { Auth } from "@supabase/auth-ui-react";
 dayjs.extend(relativeTime);
 
 type Transaction = {
@@ -19,28 +20,15 @@ function displayDate(date: dayjs.Dayjs) {
   return diff < 60 ? diff + " minutes ago" : date.fromNow();
 }
 
-function reasonsCantHaveADrink(transactions: Transaction[]): string[] {
-  let reasons: string[] = [];
-  if (transactions.length == 0) return reasons;
-  const last = lastDrinkMinutesAgo(transactions);
-  if (last < 30) reasons.push(`we had a drink ${last} minutes ago`);
-  const howMany = transactions.length;
-  if (howMany > 5) reasons.push(`we can't have more drinks, we had ${howMany}`);
-  return reasons;
-}
+type User = { user_id: string; email: string };
 
-function lastDrinkMinutesAgo(transactions: Transaction[]): number {
-  if (!transactions[0]) return -1;
-  return dayjs().diff(dayjs(transactions[0].timestamp), "minutes");
-}
-
-function canHaveADrink(transactions: Transaction[]): boolean {
-  return reasonsCantHaveADrink(transactions).length === 0;
-}
-
+// Component
 export default function Home() {
   const [transactions, setTransactions] = useState<Transaction[] | null>(null);
   const [appError, setAppError] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [now, setNow] = useState<dayjs.Dayjs>(dayjs());
+  const [lastRefreshed, setLastRefreshed] = useState<dayjs.Dayjs>(dayjs());
   console.log({ transactions });
   async function insertTransaction() {
     const { data, error } = await supabase
@@ -78,29 +66,87 @@ export default function Home() {
       .from("transactions")
       .select("id,timestamp,user_id")
       .gte("timestamp", dayjs().startOf("day").toISOString());
-    if (transactions)
+
+    if (transactions) {
       setTransactions(
         transactions.sort(
           (a, b) =>
             Number(new Date(b.timestamp)) - Number(new Date(a.timestamp))
         )
       );
+      setLastRefreshed(dayjs());
+    }
   };
 
   useEffect(() => {
     getTransactions();
+    supabase.auth.getSession().then((response) => {
+      console.log({ response });
+      if (response.error) {
+        console.log("Login error: ", response.error);
+      } else {
+        if (!response.data.session) {
+          console.log("No session found: ", response.error);
+        } else
+          setUser({
+            user_id: response.data.session.user.id,
+            email: response.data.session.user.email || "user without email",
+          });
+      }
+    });
+    setInterval(() => setNow(dayjs()), 1000 * 60);
   }, []);
+
+  function lastDrinkMinutesAgo(transactions: Transaction[]): number {
+    if (!transactions[0]) return -1;
+    return now.diff(dayjs(transactions[0].timestamp), "minutes");
+  }
+  function reasonsCantHaveADrink(transactions: Transaction[]): string[] {
+    let reasons: string[] = [];
+    if (transactions.length == 0) return reasons;
+    const last = lastDrinkMinutesAgo(transactions);
+    if (last < 30) reasons.push(`we had a drink ${last} minutes ago`);
+    const howMany = transactions.length;
+    if (howMany > 5)
+      reasons.push(`we can't have more drinks, we had ${howMany}`);
+    return reasons;
+  }
+  function canHaveADrink(transactions: Transaction[]): boolean {
+    return reasonsCantHaveADrink(transactions).length === 0;
+  }
   const canUse = transactions !== null && canHaveADrink(transactions);
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (event === "SIGNED_IN" && session) {
+      setUser({
+        user_id: session.user.id,
+        email: session.user.email || "user without email",
+      });
+    }
+  });
   return (
     <div className="flex justify-center">
       <div className="flex flex-col justify-between h-screen max-w-[800px] items-center">
         <div className="p-2 flex flex-col gap-2 items-center">
           <img className="max-w-[50%]" src={logo.src} alt="Logo" />
+          {!user ? (
+            <Auth supabaseClient={supabase} providers={[]} redirectTo="/" />
+          ) : (
+            <button
+              onClick={() =>
+                supabase.auth.signOut().then(({ error }) => {
+                  console.log("signout error", error);
+                  setUser(null);
+                })
+              }
+            >
+              Sign Out {user.email}
+            </button>
+          )}
           <h2 className="flex justify-center text-lg mb-4">Five-a-Day 😜</h2>
           {transactions === null ? (
             <p>Loading ...</p>
           ) : (
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-full">
               <p className={`${canUse ? "text-green-500" : "text-red-500"}`}>
                 {canUse
                   ? "We can order a drink! ☕️"
@@ -117,6 +163,16 @@ export default function Home() {
                 {canUse ? "Track use" : "Can't use it now"}
               </button>
               <p>Today we had {transactions.length}/5 drinks</p>
+              <div className="flex justify-between items-center">
+                <span>Last refreshed {displayDate(lastRefreshed)} </span>
+                <button
+                  className="p-1 rounded border bg-blue-400 text-white"
+                  onClick={getTransactions}
+                >
+                  Refresh
+                </button>
+              </div>
+
               <ul className="flex flex-col gap-1">
                 {transactions.map((t) => {
                   const tDate = dayjs(t.timestamp);
