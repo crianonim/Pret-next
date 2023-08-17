a517236ddc3209dd464ce9d97871b36dc3faadfe
Jan Skowronski <crianonim@gmail.com>
2023-08-17 11:50:25 +0100

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="apple-touch-icon" href="/assets/favicon.png"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
